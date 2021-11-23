e699ab13a78884614e479954fb8015bc25088337
onomdev <ikonomirei0@gmail.com>
2021-11-24 00:15:27 +0100

    twelfth commit

---
+++ store-api/starter/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
